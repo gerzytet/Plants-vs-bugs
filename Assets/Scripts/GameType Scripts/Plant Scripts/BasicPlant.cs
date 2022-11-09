a6fee57a8ff60072ced38e5ce69d70b725f75e56
TuxsetoXtreme <13951+jjs472@users.noreply.github.coecis.cornell.edu>
2022-11-08 20:07:21 -0500

    working on projectile

--- Assets/Scripts/GameType Scripts/Plant Scripts/BasicPlant.cs
+++ Assets/Scripts/GameType Scripts/Plant Scripts/BasicPlant.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 public class BasicPlant : Plant
 {
-    [SerializeField] GameObject projectile;
+    [SerializeField] Projectile projectilePrefab;
     bool canShoot = true;
     float nextShot;
 
     public override void Shoot()
     {
         nextShot = nextShot = Time.time + ((PlantInfo)gameTypeInfo).reload;
-        
+        Projectile projectile = Instantiate(projectilePrefab, transform.position, transform.rotation, transform);
+        Destroy(projectile, 5);
     }
 
     private void FixedUpdate()
